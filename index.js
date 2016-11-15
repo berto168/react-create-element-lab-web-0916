feb1b3250652172fa0be8f624fb61034c82a0d60
Robert Andrew Lin <github email address>
2016-11-15 13:37:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//Code React element here
+const list = React.createElement('ul', {className: "me__interests"},
+[
+  React.createElement('li', {}, "JavaScript"),
+  React.createElement('li', {}, "React"),
+  React.createElement('li', {}, "Movies"),
+  React.createElement('li', {}, "Ice cream")
+]);
+
+const meInReact = React.createElement('div', {className: 'me'},
+[
+  React.createElement('h1', {}, "An Awesome Person"),
+  React.createElement('p', {}, "Who is learning React"),
+  list
+])
+
+ReactDOM.render(meInReact, document.getElementById('main'));
